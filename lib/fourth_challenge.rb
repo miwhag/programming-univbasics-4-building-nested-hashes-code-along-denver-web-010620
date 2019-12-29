20e8bcc55fc9721c062a57d7be27393e5c24370e
Miwha Geschwind <miwhag@gmail.com>
2019-12-29 22:00:13 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -18,11 +18,10 @@ def fourth_challenge
             {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-   capulet[:heroine][status] = "dead"
-   
+   epic_tragedy[:montague][:heroine][status] = "dead"
+   epic_tragedy[:capulet][:hero][status] = "dead"
+
 }
-
-
   
 
 end
