bebe8d3c5cf4ebc5a43a3772a234af7d12b51eec
Miwha Geschwind <miwhag@gmail.com>
2019-12-29 21:48:12 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,9 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
         :hero_friends => [
-          :Benvolio => {
-            :name => "Benvolio", :age => "17", :attitude => "worried" },  
-          :Mercutio => { "Mercutio", :age => "18", :attitude => "hot-headed"
+            {:name => "Benvolio", :age => "17", :attitude => "worried" },  
+            {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -15,9 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        :Steven => {
-            :name => "Steven", :age => "30", :attitude => "confused" } , 
-          :Nurse => { "Nurse", :age => "44", :attitude => "worried"
+            {:name => "Steven", :age => "30", :attitude => "confused" } , 
+            {:name => "Nurse", :age => "44", :attitude => "worried"
       ]
    }
 }
